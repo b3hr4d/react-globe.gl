37a701727401a54cf9d8a560b39abf346daff222
b3hr4d <behradmusiclove@yahoo.com>
2022-09-28 19:05:11 +0300

    remove unused file

--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
-import fromKapsule from 'react-kapsule';
-import GlobeKapsule from 'globe.gl';
-import GlobePropTypes from './globe-proptypes';
+import fromKapsule from "react-kapsule"
+import GlobeKapsule from "globe.gl-fork"
+import GlobePropTypes from "./globe-proptypes"
 
-const Globe = fromKapsule(
-  GlobeKapsule,
-  {
-    methodNames: [ // bind methods
-      'pauseAnimation',
-      'resumeAnimation',
-      'pointOfView',
-      'scene',
-      'camera',
-      'renderer',
-      'postProcessingComposer',
-      'controls',
-      'getGlobeRadius',
-      'getCoords',
-      'getScreenCoords',
-      'toGeoCoords',
-      'toGlobeCoords'
-    ],
-    initPropNames: [
-      'animateIn',
-      'waitForGlobeReady',
-      'rendererConfig'
-    ]
-  }
-);
+const Globe = fromKapsule(GlobeKapsule, {
+  methodNames: [
+    // bind methods
+    "pauseAnimation",
+    "resumeAnimation",
+    "pointOfView",
+    "scene",
+    "camera",
+    "renderer",
+    "postProcessingComposer",
+    "controls",
+    "getGlobeRadius",
+    "getCoords",
+    "getScreenCoords",
+    "toGeoCoords",
+    "toGlobeCoords",
+  ],
+  initPropNames: ["animateIn", "waitForGlobeReady", "rendererConfig"],
+})
 
-Globe.displayName = 'Globe';
-Globe.propTypes = GlobePropTypes;
+Globe.displayName = "Globe"
+Globe.propTypes = GlobePropTypes
 
-export default Globe;
+export default Globe
